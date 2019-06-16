68604d47629a8b53425e51b5068cd60d8926c331
Muhammad Naufal Rizqullah <opalcool8@gmail.com>
2019-06-16 14:43:49 +0800

    update csss link contact

--- VIEW/USER/PETANI/contact.php
+++ VIEW/USER/PETANI/contact.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="utf-8">
   <title>CONTACT</title>
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/contact.css">
+  <link rel="stylesheet" href="../../CSS/main.css">
+  <link rel="stylesheet" href="../../CSS/contact.css">
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
