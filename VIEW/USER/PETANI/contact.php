2af567f373095d1a6a9425edfbabf8f59dbb3290
Muhammad Naufal Rizqullah <opalcool8@gmail.com>
2019-06-16 13:50:06 +0800

    cuma atur aja

---
+++ VIEW/USER/PETANI/contact.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>CONTACT</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+  </head>
+  <body>
+      <div class="subnavbar">
+          <div class="logo">
+          
+          </div>
+          <a class="btn">
+          <span></span>
+          <span></span>
+          <span></span>
+          </a>
+          <div class="search-box">
+            <input  class="search-txt" type="text" name="" placeholder="Type to search"/>
+            <a class="search-btn" href="#">
+             <i class="fas fa-search"></i>
+            </a>
+           </div>
+              <div class="menu">
+                  <a href="my_account.php">My Account</a>
+                  <i class="fas fa-user"></i>
+                  <a href="../../../CONTROLLER/logout.php">Logout</a>
+                  <i class="fas fa-sign-in-alt"></i>
+             </div>
+          </div>
+          <div class="navbar">
+              
+              <a class="btn">
+              <span></span>
+              <span></span>
+              <span></span>
+              </a>
+              
+                  <div class="menu">
+                      <a href="index.html">Home</a>
+                      <a href="shop.html">Shop</a>
+                      <a href="cart.html">Cart</a>
+                      <a href="contact.html">Contact</a>
+                  </div>
+              </div>     
+          <br><br><br><br><br>
+
+
+<form class="box">
+<h1 >contact us</h1>
+<input type="text" name="" placeholder="username">
+<input type="email" name="" placeholder="email">
+<input type="number" name="" placeholder="phone number">
+<input type="texti" name="" placeholder="Comment">
+<button ">Send</button>
+</form>
+  
+  </body>
+</html>
